ed1c58ceb074d72621820f10cddfde8db92297c6
zibertscrem <zibertscrem@gmail.com>
2024-01-30 20:15:50 +0200

    Fix typo in chmod keymap

--- lua/zibertscrem/remap.lua
+++ lua/zibertscrem/remap.lua
@@ -29,5 +29,5 @@ vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader>x", "<cmd>!cmod +x %<CR>", {silent = true})
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
 
