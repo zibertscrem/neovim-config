a9e412eb97ad225634b385e84534da50cab9b56e
zibertscrem <zibertscrem@gmail.com>
2024-02-07 17:44:18 +0200

    Add more LSP key bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,17 +3,20 @@ local lsp_zero = require("lsp-zero")
 lsp_zero.on_attach(function(_, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
     vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
-    vim.keymap.set("v", "<leader>vf", function() vim.lsp.buf.format() end, opts)
+    vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format({ async = true }) end, opts)
+    vim.keymap.set("v", "<leader>vf", function() vim.lsp.buf.format({ async = true }) end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
@@ -23,6 +26,7 @@ require("mason-lspconfig").setup({
         "rust_analyzer",
         "pyright",
         "lua_ls",
+        "gopls",
     },
     handlers = {
         lsp_zero.default_setup,
