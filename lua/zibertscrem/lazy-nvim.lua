c43e8ccc1bfa37ed7db9ef142b279df93385db51
zibertscrem <zibertscrem@gmail.com>
2024-02-13 21:27:48 +0200

    Add conform and lint for formatting/linting

--- lua/zibertscrem/lazy-nvim.lua
+++ lua/zibertscrem/lazy-nvim.lua
@@ -1,34 +1,70 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }, },
-    { "nvim-tree/nvim-tree.lua", dependencies = {  "nvim-tree/nvim-web-devicons", } },
-    { "rose-pine/neovim", name = "rose-pine" },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {"mbbill/undotree", tag = "rel_6.1"},
-    {'tpope/vim-fugitive'},
-    {'akinsho/toggleterm.nvim', tag = "v2.9.0", config = true},
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {"williamboman/mason.nvim"},
-    {"williamboman/mason-lspconfig.nvim"},
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'L3MON4D3/LuaSnip'},
-    {'hrsh7th/cmp-nvim-lua'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'rafamadriz/friendly-snippets'},
-    {'numToStr/Comment.nvim', opts={}, tag="v0.8.0", lazy=false},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{
+		"mbbill/undotree",
+		tag = "rel_6.1",
+	},
+	{ "tpope/vim-fugitive" },
+	{
+		"akinsho/toggleterm.nvim",
+		tag = "v2.9.0",
+		config = true,
+	},
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+	},
+	{ "neovim/nvim-lspconfig" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "hrsh7th/cmp-nvim-lua" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "rafamadriz/friendly-snippets" },
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		tag = "v0.8.0",
+		lazy = false,
+	},
+	{
+		"stevearc/conform.nvim",
+		tag = "v5.2.1",
+		opts = {},
+	},
+	{ "mfussenegger/nvim-lint" },
 })
