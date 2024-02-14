5dd4d206a64d6d1aae6013f3934b1a9e1b55cc0b
zibertscrem <zibertscrem@gmail.com>
2024-02-14 03:31:05 +0200

    Fix mason tool installer config

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -3,50 +3,52 @@ local conform = require("conform")
 local lint = require("lint")
 
 mason_installer.setup({
-    "isort",
-    "black",
-    "stylua",
-    "prettier",
-    "pyproject-flake8",
-    "mypy",
-    "impl",
+	ensure_installed = {
+		"isort",
+		"black",
+		"stylua",
+		"prettier",
+		"pyproject-flake8",
+		"mypy",
+		"impl",
+	},
 })
 
 local conform_opts = {
-    timeout_ms = 500,
-    async = false,
-    lsp_fallback = true,
+	timeout_ms = 500,
+	async = false,
+	lsp_fallback = true,
 }
 conform.setup({
-    formatters_by_ft = {
-        python = { "isort", "black" },
-        lua = { "stylua" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        jsx = { "prettier" },
-        javascript = { "prettier" },
-        less = { "prettier" },
-        markdown = { "prettier" },
-        scss = { "prettier" },
-        typescript = { "prettier" },
-        yaml = { "prettier" },
-    },
-    format_on_save = conform_opts,
+	formatters_by_ft = {
+		python = { "isort", "black" },
+		lua = { "stylua" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		jsx = { "prettier" },
+		javascript = { "prettier" },
+		less = { "prettier" },
+		markdown = { "prettier" },
+		scss = { "prettier" },
+		typescript = { "prettier" },
+		yaml = { "prettier" },
+	},
+	format_on_save = conform_opts,
 })
 lint.linters_by_ft = {
-    python = { "pflake8", "mypy" },
+	python = { "pflake8", "mypy" },
 }
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-    group = lint_augroup,
-    callback = function()
-        lint.try_lint()
-    end,
+	group = lint_augroup,
+	callback = function()
+		lint.try_lint()
+	end,
 })
 vim.keymap.set({ "n", "v" }, "<leader>vf", function()
-    conform.format(conform_opts)
+	conform.format(conform_opts)
 end)
 vim.keymap.set({ "n", "v" }, "<leader>vl", function()
-    lint.try_lint()
+	lint.try_lint()
 end)
