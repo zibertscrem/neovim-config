95e0ff516b872a95c3854a3adc7041372a7b8ceb
zibertscrem <zibertscrem@gmail.com>
2024-02-15 03:42:07 +0200

    Make transparent background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,2 +1,7 @@
-vim.cmd("colorscheme rose-pine-main")
-
+function ApplyColors()
+	vim.cmd.colorscheme("rose-pine-main")
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+ApplyColors()
