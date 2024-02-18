f896cacf4d4db0b259ff0a304e8292e802ad8ce7
zibertscrem <zibertscrem@gmail.com>
2024-02-18 23:23:43 +0200

    Format remap file

--- lua/zibertscrem/remap.lua
+++ lua/zibertscrem/remap.lua
@@ -11,13 +11,12 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-N>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "\"", "c\"\"<Esc>P")
-vim.keymap.set("v", "\'", "c\'\'<Esc>P")
+vim.keymap.set("v", '"', 'c""<Esc>P')
+vim.keymap.set("v", "'", "c''<Esc>P")
 vim.keymap.set("v", "(", "c()<Esc>P")
 vim.keymap.set("v", "[", "c[]<Esc>P")
 vim.keymap.set("v", "<", "c<><Esc>P")
 vim.keymap.set("v", "{", "c{}<Esc>P")
-
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
