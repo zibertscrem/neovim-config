318da764acbe166fa3f09d0c3622270d6cace301
zibertscrem <zibertscrem@gmail.com>
2024-02-18 23:25:31 +0200

    Format colors file

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,8 @@
 function ApplyColors()
-	vim.cmd.colorscheme("rose-pine-main")
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.colorscheme("rose-pine-main")
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
 ApplyColors()
