112fa133f0c515202bee77058f5ce39688dab61f
zibertscrem <zibertscrem@gmail.com>
2024-03-01 02:25:30 +0200

    Change autocomplete confirmation keystroke

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -103,7 +103,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
         ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
     }),
 })
