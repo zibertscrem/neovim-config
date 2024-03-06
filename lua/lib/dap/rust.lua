992f3b75a309995c403d49d55823cde7062cbfa2
zibertscrem <zibertscrem@gmail.com>
2024-03-07 00:48:30 +0200

    Make rust find debug target automatically

--- lua/lib/dap/rust.lua
+++ lua/lib/dap/rust.lua
@@ -5,19 +5,22 @@ local M = {}
 local function codelldbBin()
     return utils.masonPackagePath("codelldb") .. "/codelldb"
 end
-local function findProgram()
-    local defaultTarget = ""
-    if vim.g.dap_rust_target then
-        defaultTarget = vim.g.dap_rust_target
+local function extractTarget()
+    local cwd = vim.fn.getcwd()
+    local cargoFile = cwd .. "/Cargo.toml"
+    if vim.fn.filereadable(cargoFile) ~= 1 then
+        vim.print("Cannot find Cargo.toml. Search path: " .. cargoFile)
+        return nil
     end
-    local target = vim.fn.input({
-        prompt = "Target: ",
-        default = defaultTarget,
-    })
-    target = vim.trim(target)
-    if target == "" then
+    local cargoContent = vim.fn.system({ "cat", cargoFile })
+    return string.match(cargoContent, '%[package%].-name%s*=%s*"([^"]+)"')
+end
+local function findProgram()
+    local target = extractTarget()
+    if not target or target == "" then
         return dap.ABORT
     end
+    vim.fn.system({ "cargo", "clean" })
     vim.fn.system({ "cargo", "build" })
     local debugTarget = vim.fn.getcwd() .. "/target/debug/" .. target
     if vim.fn.executable(debugTarget) ~= 1 then
@@ -49,8 +52,9 @@ M.configuration = {
     {
         type = "rust",
         request = "launch",
-        name = "Debug program",
+        name = "Debug target",
         program = findProgram,
+        cwd = vim.fn.getcwd,
         args = utils.supplyArguments,
     },
 }
