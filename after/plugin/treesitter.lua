f7abb20e8341c95755c1d13b2615da6bdd9b18df
zibertscrem <zibertscrem@gmail.com>
2024-06-26 06:31:23 +0300

    Correct go to context

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -40,6 +40,6 @@ vim.keymap.set("n", "<leader>fc", function()
     require("treesitter-context").toggle()
 end)
 
-vim.keymap.set("n", "<leader>gc", function()
+vim.keymap.set({"n", "v"}, "<leader>gc", function()
     require("treesitter-context").go_to_context(vim.v.count1)
 end)
