842876dd6e9a4faa4deebb1d5f376543aecd35a7
zibertscrem <zibertscrem@gmail.com>
2025-06-06 20:09:00 +0300

    Upgrade to fresh nvim 0.11

--- after/plugin/lorem.lua
+++ after/plugin/lorem.lua
@@ -1,1 +1,5 @@
-require("lorem")
+require("lorem").opts({
+    sentence_length = "mixedShort",
+    comma_chance = 0.1,
+    max_commas = 2,
+})
--- lua/zibertscrem/lazy-nvim.lua
+++ lua/zibertscrem/lazy-nvim.lua
@@ -108,7 +108,7 @@ require("lazy").setup({
             },
             {
                 "mrcjkb/rustaceanvim",
-                version = "^4", -- Recommended
+                version = "^6", -- Recommended
                 lazy = false, -- This plugin is already lazy
             },
         },
