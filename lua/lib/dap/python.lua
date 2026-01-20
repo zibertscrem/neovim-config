1a3c674a344d9536dc7427ace9e312b055d3a12e
zibertscrem <zibertscrem@gmail.com>
2026-01-20 17:58:59 +0200

    Fix DAP for python implementation

--- lua/lib/dap/python.lua
+++ lua/lib/dap/python.lua
@@ -4,121 +4,121 @@ local utils = require("lib.dap.utils")
 local M = {}
 
 local function projectRoot()
-    return utils.rootDir({
-        "pyproject.toml",
-        "requirements.txt",
-        "setup.py",
-        "setup.cfg",
-        "poetry.toml",
-    }) or vim.fn.getcwd()
+	return utils.rootDir({
+		"pyproject.toml",
+		"requirements.txt",
+		"setup.py",
+		"setup.cfg",
+		"poetry.toml",
+	}) or vim.fn.getcwd()
 end
 local function pythonPath()
-    -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-    -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-    -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-    local activeEnv = vim.fn.environ()["VIRTUAL_ENV"]
-    if activeEnv ~= nil then
-        return activeEnv .. "/bin/python"
-    end
+	-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+	-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+	-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+	local activeEnv = vim.fn.environ()["VIRTUAL_ENV"]
+	if activeEnv ~= nil then
+		return activeEnv .. "/bin/python"
+	end
 
-    if vim.fn.executable("poetry") == 1 then
-        local poetryPython = vim.fn.system({ "poetry", "env", "info", "-e" })
-        if vim.fn.executable(poetryPython) == 1 then
-            return poetryPython
-        end
-    end
-    local root = projectRoot()
-    if vim.fn.executable(root .. "/venv/bin/python") == 1 then
-        return root .. "/venv/bin/python"
-    end
-    if vim.fn.executable(root .. "/.venv/bin/python") == 1 then
-        return root .. "/.venv/bin/python"
-    end
-    return "python"
+	if vim.fn.executable("poetry") == 1 then
+		local poetryPython = vim.fn.system({ "poetry", "env", "info", "-e" })
+		if vim.fn.executable(poetryPython) == 1 then
+			return poetryPython
+		end
+	end
+	local root = projectRoot()
+	if vim.fn.executable(root .. "/venv/bin/python") == 1 then
+		return root .. "/venv/bin/python"
+	end
+	if vim.fn.executable(root .. "/.venv/bin/python") == 1 then
+		return root .. "/.venv/bin/python"
+	end
+	return "python"
 end
 local function getCurrentModulePath()
-    return vim.fn.expand("%:.:h:gs?/?.?")
+	return vim.fn.expand("%:.:h:gs?/?.?")
 end
 local function getCustomModulePath()
-    local defaultModulePath = getCurrentModulePath()
-    if vim.g.dap_python_module then
-        defaultModulePath = vim.g.dap_python_module
-    end
-    local modulePath = vim.fn.input({
-        prompt = "Module dotted path: ",
-        default = defaultModulePath,
-    })
-    modulePath = vim.trim(modulePath)
-    vim.g.dap_python_module = modulePath
-    if modulePath == "" then
-        return nil
-    end
-    return modulePath
+	local defaultModulePath = getCurrentModulePath()
+	if vim.g.dap_python_module then
+		defaultModulePath = vim.g.dap_python_module
+	end
+	local modulePath = vim.fn.input({
+		prompt = "Module dotted path: ",
+		default = defaultModulePath,
+	})
+	modulePath = vim.trim(modulePath)
+	vim.g.dap_python_module = modulePath
+	if modulePath == "" then
+		return nil
+	end
+	return modulePath
 end
 M.adapter = function(cb, config)
-    if config.request == "attach" then
-        ---@diagnostic disable-next-line: undefined-field
-        local port = (config.connect or config).port
-        ---@diagnostic disable-next-line: undefined-field
-        local host = (config.connect or config).host or "127.0.0.1"
-        cb({
-            type = "server",
-            port = assert(port, "`connect.port` is required for a python `attach` configuration"),
-            host = host,
-            options = {
-                source_filetype = "python",
-            },
-        })
-    else
-        cb({
-            type = "executable",
-            command = utils.masonPackagePath("debugpy") .. "/venv/bin/python",
-            args = { "-m", "debugpy.adapter" },
-            options = {
-                source_filetype = "python",
-            },
-        })
-    end
+	if config.request == "attach" then
+		---@diagnostic disable-next-line: undefined-field
+		local port = (config.connect or config).port
+		---@diagnostic disable-next-line: undefined-field
+		local host = (config.connect or config).host or "127.0.0.1"
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = host,
+			options = {
+				source_filetype = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = utils.masonPackagePath("debugpy") .. "/debugpy-adapter",
+			args = {},
+			options = {
+				source_filetype = "python",
+			},
+		})
+	end
 end
 
 M.configuration = {
-    {
-        justMyCode = false,
-        type = "python",
-        request = "launch",
-        name = "Launch file",
-        program = "${file}", -- This configuration will launch the current file if used.
-        args = utils.supplyArguments,
-        cwd = projectRoot,
-        pythonPath = pythonPath,
-    },
-    {
-        justMyCode = false,
-        type = "python",
-        request = "launch",
-        name = "Launch module",
-        module = getCurrentModulePath,
-        args = utils.supplyArguments,
-        cwd = projectRoot,
-        pythonPath = pythonPath,
-    },
-    {
-        justMyCode = false,
-        type = "python",
-        request = "launch",
-        name = "Launch module (custom)",
-        module = getCustomModulePath,
-        args = utils.supplyArguments,
-        cwd = projectRoot,
-        pythonPath = pythonPath,
-    },
+	{
+		justMyCode = false,
+		type = "python",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}", -- This configuration will launch the current file if used.
+		args = utils.supplyArguments,
+		cwd = projectRoot,
+		pythonPath = pythonPath,
+	},
+	{
+		justMyCode = false,
+		type = "python",
+		request = "launch",
+		name = "Launch module",
+		module = getCurrentModulePath,
+		args = utils.supplyArguments,
+		cwd = projectRoot,
+		pythonPath = pythonPath,
+	},
+	{
+		justMyCode = false,
+		type = "python",
+		request = "launch",
+		name = "Launch module (custom)",
+		module = getCustomModulePath,
+		args = utils.supplyArguments,
+		cwd = projectRoot,
+		pythonPath = pythonPath,
+	},
 }
 M.neotest = require("neotest-python")({
-    dap = {
-        justMyCode = false,
-        cwd = projectRoot,
-    },
-    python = pythonPath,
+	dap = {
+		justMyCode = false,
+		cwd = projectRoot,
+	},
+	python = pythonPath,
 })
 
 return M
--- lua/lib/dap/utils.lua
+++ lua/lib/dap/utils.lua
@@ -1,44 +1,41 @@
 local mason_path = require("mason-core.path")
+local mason_settings = require("mason.settings")
 local M = {}
 
 function M.masonPackagePath(pkg)
-    return mason_path.package_prefix(pkg)
-end
-
-function M.masonSharePackagePath(pkg)
-    return mason_path.share_prefix(pkg)
+	return mason_path.concat({ mason_settings.current.install_root_dir, "packages", pkg })
 end
 
 function M.rootDir(files)
-    local found = vim.fs.find(files, { upward = true, path = vim.api.nvim_buf_get_name(0) })[1]
-    if found then
-        return vim.fs.dirname(found)
-    end
-    return nil
+	local found = vim.fs.find(files, { upward = true, path = vim.api.nvim_buf_get_name(0) })[1]
+	if found then
+		return vim.fs.dirname(found)
+	end
+	return nil
 end
 
 function M.supplyArguments()
-    local defaultArgs = ""
-    if vim.g.dap_arguments then
-        defaultArgs = vim.g.dap_arguments
-    end
-    local args = vim.fn.input({
-        prompt = "Arguments: ",
-        default = defaultArgs,
-    })
-    vim.g.dap_arguments = args
-    if args == "" then
-        return nil
-    end
-    return vim.split(args, " ")
+	local defaultArgs = ""
+	if vim.g.dap_arguments then
+		defaultArgs = vim.g.dap_arguments
+	end
+	local args = vim.fn.input({
+		prompt = "Arguments: ",
+		default = defaultArgs,
+	})
+	vim.g.dap_arguments = args
+	if args == "" then
+		return nil
+	end
+	return vim.split(args, " ")
 end
 
 function M.addOrCreate(tbl, extension)
-    local existing = tbl
-    if existing == nil then
-        existing = {}
-    end
-    return vim.tbl_extend("keep", existing, extension)
+	local existing = tbl
+	if existing == nil then
+		existing = {}
+	end
+	return vim.tbl_extend("keep", existing, extension)
 end
 
 return M
